00aed3860caee41aa8d13ed36e003f4a6a1f0ec3
sebastiancs24 <sebastiancs2410cuevas@gmail.com>
2022-11-14 18:58:36 -0400

    programas nuevos

--- programa1-1.c
+++ programa1-1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /* Programa 1.1
 El siguiente es el primer programa escrito en el lenguaje C. */
-void main (void)
+int main ()
 {
-printf( “Mi primer programa en C” );}
+printf( "Mi primer programa en C");}
--- programa2-1.c
+++ programa2-1.c
@@ -3,11 +3,11 @@
 El programa, al recibir como dato el promedio de un alumno en un curso
 ➥universitario, escribe aprobado si su promedio es mayor o igual a 6.
 PRO: variable de tipo real. */
-void main(void)
+int main()
 {
 float PRO;
-printf(“ingrese el promedio del alumno: “);
-scanf(“%f”, &PRO);
+printf("ingrese el promedio del alumno: ");
+scanf("%f", &PRO);
 if (PRO >= 6)
-printf(“\nAprobado”);
+printf("\nAprobado");
 }
