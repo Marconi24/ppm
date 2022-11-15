8ccaca3f253a1bfaeb98646d55b6ede0204e30b4
sebastiancs24 <sebastiancs2410cuevas@gmail.com>
2022-11-14 23:29:46 -0400

    todos los programas de este dia

--- programa3-10.c
+++ programa3-10.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <math.h>
-/* Pares e impares. 
-El programa, al recibir como datos N números enteros, obtiene la suma de los
-➥números pares y calcula el promedio de los impares.
-I, N, NUM, SPA, SIM, CIM: variables de tipo entero. */
-void main(void)
-{
-int I, N, NUM, SPA = 0, SIM = 0, CIM = 0;
-printf("Ingrese el número de datos que se van a procesar:\t");
-scanf("%d", &N);
-if (N > 0)
-{
-for (I=1; I <= N; I++)
-{
-printf("\nIngrese el número %d: ", I);
-scanf("%d", &NUM);
-if (NUM)
-if (pow(-1, NUM) > 0)
-SPA = SPA + NUM;
-else
-{
-SIM = SIM + NUM;
-CIM++;
-}
-}
-printf("\n La suma de los números pares es: %d", SPA);
-printf("\n El promedio de números impares es: %5.2f", (float)(SIM / CIM);
 
-}
-else
-printf("\n El valor de N es incorrecto");
+/*Pares e impares.
+El problema, al recibir como datos N numeros enteros, obtiene la suma de los
+numeros pares y calcula el promedio de los impares.
+I, N, NUM, SPA, SIM, CIM: variables de tipo entero. */
+
+void main(void) //Sirve como punto de entrada del programa
+{
+    int I, N, NUM, SPA = 0, SIM = 0, CIM = 0; //Define variables numericas con numeros de tipo entero
+    printf("Ingrese el numero de datos que se van a procesar:\t"); //Presenta en la pantalla algun dato
+    scanf("%d", &N); //Lee caracteres ingresados desde el teclado
+    if (N > 0) //Se utiliza para establece una condicion
+    {
+        for (I=1; I <= N; I++) /* Hace que las instrucciones se repitan el número de veces que 
+        le decimos, normalmente le ponemos un número ( o el valor de una variable o una constante) */
+        {
+            printf("\nIngrese el numero %d: ", I); 
+            scanf("%d", &NUM);
+            if (NUM) //Se utiliza para establece una condicion
+                if (pow(-1, NUM) > 0)
+                    SPA = SPA + NUM;
+                else //Si la condicion es falsa pasa a esta linea de codigo
+                    {
+                        SIM = SIM + NUM; //Operacion aritmetica de suma dos variables
+                        CIM++; //Incremento en uno del conteo
+                    }
+        }
+        printf("\n La suma de los numeros pares es: %d", SPA);
+        printf("\n El promedio de numeros impares es: %5.2f", (float)(SIM / CIM));
+    }
+    else
+    printf("\n El valor de N es incorrecto");
 }
--- programa3-12.c
+++ programa3-12.c
@@ -2,27 +2,27 @@
 #include <math.h>
 /* Serie de ULAM.
 El programa, al recibir como dato un entero positivo, obtiene y escribe
-➥la serie de ULAM.
+la serie de ULAM.
 NUM: variable de tipo entero. */
-void main(void)
+void main(void) //Sirve como punto de entrada del programa
 {
-int NUM;
-printf("Ingresa el número para calcular la serie: ");
-scanf("%d", &NUM);
-if (NUM > 0)
-{
-     printf("\nSerie de ULAM\n");
-         printf("%d \t", NUM);
-          while (NUM != 1)
-{
-if (pow(-1, NUM) > 0)
-NUM = NUM / 2;
-else
-NUM = NUM * 3 + 1;
-printf(“%d \t”, NUM);
-}
-                 }
-                   else
-                   
-printf("\n NUM debe ser un entero positivo");
+    int NUM; //Define variables numericas con numeros de tipo entero
+    printf("Ingresa el numero para calcular la serie: "); //Presenta en la pantalla algun dato
+    scanf("%d", &NUM); //Lee caracteres ingresados des
+    if (NUM > 0) //Se utiliza para establece una condicion
+    {
+        printf("\nSerie de ULAM\n");
+        printf("%d \t", NUM);
+        while (NUM != 1) /*Permite selccionar el bloque de instucciones que se va a 
+        ejecutar dentro de varias opciones */
+        {
+            if (pow(-1, NUM) > 0)
+                NUM = NUM / 2;
+            else
+                NUM = NUM * 3 + 1;
+                printf("%d \t", NUM);
+        }
+    }
+    else //Si la condicion es falsa pasa a esta linea de codigo
+    printf("\n NUM debe ser un entero positivo");
 }
